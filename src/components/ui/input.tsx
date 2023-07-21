49047bc36b093b053aa3029c688233629d63cdf6
Pratiksha Sontakke <pratiksha.sontakke@torinit.ca>
2023-07-22 00:42:38 +0530

    minor fix

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
+
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   error?: string;
